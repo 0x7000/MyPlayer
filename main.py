15eb2bc37ec50fc37b433858fc6d6b63b148974a
command <rekreb81@gmail.com>
2022-05-01 19:18:03 +0300

    init01

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # https://github.com/20tab/mpg123-python
 # icons tango icon theme
 
+import tkinter.ttk as ttk
 import tkinter
 from tkinter import *
 from tkinter import messagebox
@@ -42,6 +43,34 @@ Frm2 = tkinter.Frame(Pencere)
 Frm2.place(relx=0.015, rely=0.27, relheight=0.72, relwidth=0.97)
 Frm2.configure(relief="groove")
 Frm2.configure(border=2)
+
+TButton1 = ttk.Button(Frm1)
+TButton1.place(relx=0.01, rely=0.71, height=44, width=44)
+TButton1.configure(text="Play")
+
+TButton2 = ttk.Button(Frm1)
+TButton2.place(relx=0.13, rely=0.71, height=44, width=44)
+TButton2.configure(text="Stop")
+
+TButton3 = ttk.Button(Frm1)
+TButton3.place(relx=0.25, rely=0.71, height=44, width=44)
+TButton3.configure(text="Next")
+
+TButton4 = ttk.Button(Frm1)
+TButton4.place(relx=0.37, rely=0.71, height=44, width=44)
+TButton4.configure(text="Prev")
+
+TButton5 = ttk.Button(Frm1)
+TButton5.place(relx=0.49, rely=0.71, height=44, width=44)
+TButton5.configure(text="Pause")
+
+TButton6 = ttk.Button(Frm1)
+TButton6.place(relx=0.88, rely=0.71, height=44, width=44)
+TButton6.configure(text="Load")
+
+TProgressbar1 = ttk.Progressbar(Frm1)
+TProgressbar1.place(relx=0.013, rely=0.55, relwidth=0.978, relheight=0.0, height=16)
+TProgressbar1.configure(length="560")
 
 
 if __name__ == '__main__':
